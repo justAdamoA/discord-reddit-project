ffb5067d5cfb60d557de3bf4eac897faeb4366eb
justAdamoA <info@justadamoa.com>
2020-11-08 12:59:10 +0100

    basic command handling and config

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client } = require('discord.js');
+
+const config = {
+  discord: {
+    token: process.env.DISCORD_CLIENT_TOKEN,
+  },
+  commands: {
+    prefix: process.env.DISCORD_COMMAND_PREFIX || '!',
+  },
+};
+
+const client = new Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', (message) => {
+  if (!message.content.startsWith(config.commands.prefix)) {
+    return;
+  }
+
+  const args = message.content.slice(1).split(/\s+/);
+  const command = args.shift();
+
+  if (command === 'help') {
+    message.channel.send('help message');
   }
 });
 
-client.login(process.env.DISCORD_CLIENT_TOKEN);
+client.login(config.discord.token);
